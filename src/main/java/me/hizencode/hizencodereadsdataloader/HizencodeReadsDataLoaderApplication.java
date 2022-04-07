527ec93f98fac76877748bc80ed9c59153f31786
MichaelAntropov <mikhael.antropov@gmail.com>
2022-04-07 22:18:25 +0300

    Initialize project

---
+++ src/main/java/me/hizencode/hizencodereadsdataloader/HizencodeReadsDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package me.hizencode.hizencodereadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HizencodeReadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HizencodeReadsDataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hizencode/hizencodereadsdataloader/HizencodeReadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hizencode.hizencodereadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HizencodeReadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
